dd946bbc213487ae9d24f2136aad278e7f8923fb
Maxim Zimin <Makmizima398@mail.ru>
2022-05-25 22:11:35 +0300

    first axis

--- TimeManagement/TimeManagement/MainWindow.xaml.cs
+++ TimeManagement/TimeManagement/MainWindow.xaml.cs
@@ -23,6 +23,55 @@ namespace TimeManagement
         public MainWindow()
         {
             InitializeComponent();
+
+            // Рисование осей Эффективности и времени
+            int X0 = 10;
+            int Y0 = 290;
+
+            // OX
+            Line line = new Line();
+            line.Stroke = new SolidColorBrush(Colors.Black);
+            line.X1 = X0;
+            line.Y1 = Y0;
+            line.X2 = 490;
+            line.Y2 = Y0;
+            line.StrokeThickness = 1;
+            CNV.Children.Add(line);
+
+            // OY
+            Line line1 = new Line();
+            line1.Stroke = new SolidColorBrush(Colors.Black);
+            line1.X1 = X0;
+            line1.Y1 = Y0;
+            line1.X2 = X0;
+            line1.Y2 = 10;
+            line1.StrokeThickness = 1;
+            CNV.Children.Add(line1);
+            
+            for (int i = 30; i <= 480; i += 30)
+            {
+                Line line2 = new Line();
+                line2.Stroke = new SolidColorBrush(Colors.Black);
+                line2.StrokeThickness = 1;
+                line2.X1 = X0 + i;
+                line2.Y1 = Y0 - 5;
+                line2.X2 = X0 + i;
+                line2.Y2 = Y0 + 5;
+                CNV.Children.Add(line2);
+            }
+            for (int i = 40; i <= 280; i += 40)
+            {
+                Line line2 = new Line();
+                line2.Stroke = new SolidColorBrush(Colors.Black);
+                line2.StrokeThickness = 1;
+                line2.X1 = X0 + 5;
+                line2.Y1 = Y0 - i;
+                line2.X2 = X0 - 5;
+                line2.Y2 = Y0 - i;
+                CNV.Children.Add(line2);
+            }
         }
+
+
     }
 }
